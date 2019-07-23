f64b6657445505ebf04afcfc819baa537139f237
neulhan <sting7675@gmail.com>
2019-07-23 15:10:46 +0900

    aaskld

--- a.py
+++ a.py
@@ -1,2 +1,20 @@
+<<<<<<< HEAD
 def a():
     print()
+=======
+def func():
+    print("hello git")
+
+
+def practice1():
+    print("Hi")
+
+
+def ccc():
+    pass
+
+
+def ab():
+    print("branch2에서 수정된 내용입니다")
+
+>>>>>>> branch1
