e638c814d6a662ef4540840844f17cb59835d52b
neulhan <sting7675@gmail.com>
2019-07-23 15:16:31 +0900

    sadsd

--- a.py
+++ a.py
@@ -1,20 +1,5 @@
-<<<<<<< HEAD
-def a():
-    print()
-=======
-def func():
-    print("hello git")
-
-
-def practice1():
-    print("Hi")
-
-
-def ccc():
-    pass
-
 
 def ab():
-    print("branch2에서 수정된 내용입니다")
+    print("error1 에서 수정된 내용입니다")
 
->>>>>>> branch1
+
